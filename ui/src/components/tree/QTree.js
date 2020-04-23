2e4bf9cc4641bae6f2905fce8bb3f71c582f8cab
Amagad Serry <amgad@benchmarklabs.io>
2020-04-23 11:18:31 +0200

    added expanded class modifier to q-tree__node-header when expanded

--- ui/src/components/tree/QTree.js
+++ ui/src/components/tree/QTree.js
@@ -484,7 +484,8 @@ export default Vue.extend({
           class: {
             'q-tree__node--link q-hoverable q-focusable': meta.link,
             'q-tree__node--selected': meta.selected,
-            'q-tree__node--disabled': meta.disabled
+            'q-tree__node--disabled': meta.disabled,
+            'q-tree__node--expanded': meta.expanded
           },
           attrs: { tabindex: meta.link ? 0 : -1 },
           on: {
